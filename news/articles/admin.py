3641746afc218dca57f0ff329fb9ad3fb7cafff8
Trevor <78657977+dupls13@users.noreply.github.com>
2022-07-21 09:25:33 -0700

    Updated and finished Newspaper app
    Uploading files

--- news/articles/admin.py
+++ news/articles/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 from . import models
 # Register your models here.
-admin.site.register(models.Article)
+
+class CommentInline(admin.StackedInline):
+    model = models.Comment 
+    
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline, 
+    ]
+
+
+admin.site.register(models.Article, ArticleAdmin)
+admin.site.register(models.Comment)
--- news/articles/views.py
+++ news/articles/views.py
@@ -2,6 +2,7 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 from . import models 
@@ -25,7 +26,12 @@ class ArticleDeleteView(DeleteView):
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
     
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = models.Article 
     template_name ='article_new.html'
     fields =['title', 'body', 'author', ]
+    login_url = 'login'
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
